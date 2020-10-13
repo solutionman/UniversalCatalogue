27ba904219727186f03dfc4a79f979222a8621e4
lom <lom@ssvet.info>
2020-10-13 18:42:14 +0700

    path

--- backend/sql/queries.sql
+++ backend/sql/queries.sql
@@ -1,1 +1,8 @@
 create database catalog;
+
+SELECT * FROM catalog;
+SELECT catalogid, * FROM catalog_field;
+
+INSERT INTO catalog VALUES (1, 'some');
+INSERT INTO catalog_field values (1, 1, 'fieldname');
+
